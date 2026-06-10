0aca5486229619c2d9ec5075e6ab5e41a6893219
vipal21 <vipalkharva99@gmail.com>
2026-06-10 07:56:39 -0600

    initial Commit

---
+++ LocalEventsExplorer/LocalEventsExplorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalEventsExplorer
+//
+//  Created by vipal on 2026-06-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
