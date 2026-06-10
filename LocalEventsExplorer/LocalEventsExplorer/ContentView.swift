ca4190cb021a16759dab9158d1d5b39862c3c2d6
vipal21 <vipalkharva99@gmail.com>
2026-06-10 08:41:55 -0600

    Data stored in coreData and fetched.

--- LocalEventsExplorer/LocalEventsExplorer/ContentView.swift
+++ LocalEventsExplorer/LocalEventsExplorer/ContentView.swift
@@ -41,10 +41,68 @@ final class ContentViewModel {
         do {
               let data2 = try Data(contentsOf: url)
             let reult =   try JSONDecoder().decode([Event].self, from: data2)
-             print(reult.count)
+            saveDatainCoreData(events: reult)
+            getDataFromCoredata()
+
+
         } catch let error {
             print(error.localizedDescription)
         }
+
+
+    }
+    func saveDatainCoreData(events:[Event]) {
+        do {
+            for event in events {
+                let entity = EventEntity(context: self.container.viewContext)
+
+                entity.id = Int64(event.id)
+                entity.title = event.title
+                entity.venue = event.venue
+                entity.venueImage = event.venueImage
+                entity.eventDescription = event.eventDescription
+                entity.eventImages = event.eventImages.joined(separator: ",")
+                entity.date = event.date
+                entity.startTime = event.startTime
+                entity.endTime = event.endTime
+                entity.latitude = event.latitude
+                entity.longitude = event.longitude
+                entity.isFavourite = event.isFavourite
+            }
+
+            try self.container.viewContext.save()
+        } catch let error {
+            print (error)
+        }
+
+    }
+    func getDataFromCoredata(){
+        do {
+            let request = EventEntity.fetchRequest()
+            let entities = try self.container.viewContext.fetch(request)
+
+            let data =  entities.map {
+                Event(
+                    id: $0.id,
+                    title: $0.title ?? "",
+                    venue: $0.venue ?? "",
+                    venueImage: $0.venueImage ?? "",
+                    eventDescription: $0.eventDescription ?? "",
+                    eventImages: ($0.eventImages ?? "").components(separatedBy: ","),
+                    date: $0.date ?? "",
+                    startTime: $0.startTime ?? "",
+                    endTime: $0.endTime ?? "",
+                    latitude: $0.latitude,
+                    longitude: $0.longitude,
+                    isFavourite: $0.isFavourite
+                )
+            }
+            print (data)
+        } catch let error {
+            print (error)
+        }
+
+
 
 
     }
